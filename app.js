08916d6029137db74aaa078bac8b80df8510a478
carlos <rogersilva600@gmail.com>
2024-01-29 19:58:01 -0300

    Desafios de funções de retorno ALURA.

---
+++ app.js
@@ -0,0 +1,40 @@
+function OlaMundo() {
+    return 'Olá Mundo';
+}
+console.log(OlaMundo());
+
+function nome(nome) {
+    return "Olá, " + nome;
+}
+console.log(nome('mateus'));
+
+function numero(numero1,) {
+    return numero1 * 2;
+}
+console.log(numero(3));
+
+function numeroMedia(num1, num2, num3,) {
+    return (num1 + num2 + num3) / 3;
+}
+console.log(numeroMedia(5, 8, 6));
+
+function numeroMedia(num1, num2, num3) {
+    return (num1 + num2 + num3) / 3;
+}
+console.log(numeroMedia(5, 8, 6));
+
+function NumeroMaior(number1, number2) {
+    if (number1 > number2) {
+        return number1;
+    }
+    else (number1 < number2) 
+    {
+        return number2;
+    }
+}
+console.log (NumeroMaior(3,4));
+
+function numeroRetorna(Numerozinho1) {
+return Numerozinho1*Numerozinho1;
+}
+console.log (numeroRetorna(11));
